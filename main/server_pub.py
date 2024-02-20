7d2204104bd8d59bd137bf30373bb07a36ab634f
luannt0801 <ntl080102@gmail.com>
2024-02-21 00:10:55 +0700

    deleted

--- main/server_pub.py
+++ main/server_pub.py
@@ -1,45 +1,26 @@
+import paho.mqtt.publish as publish
+import paho.mqtt.subscribe as subscribe
+import paho.mqtt.client
 import time
-import paho.mqtt.client as mqtt
+import threading
 
-def on_publish(client, userdata, mid, reason_code, properties):
-    # reason_code and properties will only be present in MQTTv5. It's always unset in MQTTv3
-    try:
-        userdata.remove(mid)
-    except KeyError:
-        print("on_publish() is called with a mid not present in unacked_publish")
-        print("This is due to an unavoidable race-condition:")
-        print("* publish() return the mid of the message sent.")
-        print("* mid from publish() is added to unacked_publish by the main thread")
-        print("* on_publish() is called by the loop_start thread")
-        print("While unlikely (because on_publish() will be called after a network round-trip),")
-        print(" this is a race-condition that COULD happen")
-        print("")
-        print("The best solution to avoid race-condition is using the msg_info from publish()")
-        print("We could also try using a list of acknowledged mid rather than removing from pending list,")
-        print("but remember that mid could be re-used !")
+broker_host = "emqx.io"
+broker_name = "broker.emqx.io"
+# broker_name = "192.168.139.129"
+n_round = 0
+def send_task(task_name, client, this_client_id):
+    # publish.single(topic="dynamicFL/cmd", payload=task_name, hostname=broker_name, client_id=server_id)
+    print_log("publish to " + "dynamicFL/req/"+this_client_id)
+    client.publish(topic="dynamicFL/req/"+this_client_id, payload=task_name)
+    
+def send_model(path, client, this_client_id):
+    f = open(path, "rb")
+    data = f.read()
+    f.close()
+    print_log(len(data))
+    print_log("publish to " + "dynamicFL/model/"+this_client_id)
+    client.publish(topic="dynamicFL/model/"+this_client_id, payload=data)
 
-unacked_publish = set()
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_publish = on_publish
 
-mqttc.user_data_set(unacked_publish)
-mqttc.connect("mqtt.eclipseprojects.io")
-mqttc.loop_start()
 
-# Our application produce some messages
-msg_info = mqttc.publish("paho/test/topic", "my message", qos=1)
-unacked_publish.add(msg_info.mid)
 
-msg_info2 = mqttc.publish("paho/test/topic", "my message2", qos=1)
-unacked_publish.add(msg_info2.mid)
-
-# Wait for all message to be published
-while len(unacked_publish):
-    time.sleep(0.1)
-
-# Due to race-condition described above, the following way to wait for all publish is safer
-msg_info.wait_for_publish()
-msg_info2.wait_for_publish()
-
-mqttc.disconnect()
-mqttc.loop_stop()
